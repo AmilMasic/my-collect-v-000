e7cade05b4e095bb25230b16064652573568c18d
Amil Masic <amil.masic@gmail.com>
2018-09-12 14:23:59 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,13 @@
 def my_collect(array)
   collection = []
+  i = 0
+  while i < collection.length
+    yield (collection[i])
+    i+=1
+  end
 end
-# my_collect(collection) do |lang|
-#   lang.upcase
-# end
+    
+end
+my_collect(collection) do |lang|
+  lang.upcase
+end
