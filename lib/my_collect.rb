ce6d0c34ac6ac721b8a8c3e0e6d95abed589afb1
Amil Masic <amil.masic@gmail.com>
2018-09-12 14:29:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 
-def my_collect(array)
+def my_collect(languages)
 
   i = 0
-  while i < array.length
-    yield (array[i])
+  while i < languages.length
+    yield (languages[i])
     i+=1
   end
 end
